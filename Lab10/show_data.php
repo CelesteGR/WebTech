79afdcb94db23518887cb52bf7dbee20aba32133
CelesteGR <113205772+CelesteGR@users.noreply.github.com>
2023-11-05 23:05:41 -0600

    Update show_data.php

--- Lab10/show_data.php
+++ Lab10/show_data.php
@@ -9,8 +9,10 @@ $data_array = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 if ($data_array === false) {
     echo 'Error reading the file.';
 } else {
-    // Create an HTML table to display the data
-    echo '<table>';
+    // Initialize an array to store the parsed data
+    $parsedFile = [];
+
+    // Parse the data into the $parsedFile array
     foreach ($data_array as $line) {
         // Split each line into first name and last name using a space as the delimiter
         $name_parts = explode(' ', $line);
@@ -19,12 +21,22 @@ if ($data_array === false) {
         if (count($name_parts) >= 2) {
             $first_name = $name_parts[0];
             $last_name = $name_parts[count($name_parts) - 1];
-            
-            echo '<tr><td>' . $first_name . '</td><td>' . $last_name . '</td></tr>';
+            $parsedFile[] = ['first_name' => $first_name, 'last_name' => $last_name];
         }
     }
+
+    // Create an HTML table to display the data
+    echo '<table>';
+    
+    // Header row
+    echo '<tr><th>First Name</th><th>Last Name</th></tr>';
+    
+    // Display data in the table
+    foreach ($parsedFile as $entry) {
+        echo '<tr><td>' . $entry['first_name'] . '</td><td>' . $entry['last_name'] . '</td></tr>';
+    }
+    
     echo '</table>';
 }
-
 ?>
 
